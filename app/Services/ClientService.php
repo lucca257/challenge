f777e85296bbe885bee58a6de938160c42d7d66d
pedro lucca <pedro.lucca@live.estacio.br>
2020-10-19 20:54:53 -0300

    refactor: extendendo BaseService em ClientService.php

--- app/Services/ClientService.php
+++ app/Services/ClientService.php
@@ -6,37 +6,12 @@ namespace App\Services;
 
 use App\Repositories\ClientRepository;
 
-class ClientService
+class ClientService extends BaseService
 {
     protected $clientRepository;
 
     public function __construct(ClientRepository $clientRepository)
     {
-        $this->clientRepository = $clientRepository;
-    }
-
-    public function all()
-    {
-        return $this->clientRepository->all();
-    }
-
-    public function save(array $attributes)
-    {
-        return $this->clientRepository->save($attributes);
-    }
-
-    public function update(array $attributes, int $id)
-    {
-        return $this->clientRepository->update($id, $attributes);
-    }
-
-    public function destroy(int $id)
-    {
-        return $this->clientRepository->destroy($id);
-    }
-
-    public function find(int $id)
-    {
-        return $this->clientRepository->find($id);
+        parent::__construct($clientRepository);
     }
 }
