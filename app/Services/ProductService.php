77c246a0fad7c2aa569db8d1fe8c4ccb7f76c497
pedro lucca <pedro.lucca@live.estacio.br>
2020-10-19 20:50:43 -0300

    refactor: utilizando baseservice em ProductService.php

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -4,37 +4,12 @@ namespace App\Services;
 
 use App\Repositories\ProductRepository;
 
-class ProductService
+class ProductService extends BaseService
 {
     protected $productRepository;
 
     public function __construct(ProductRepository $productRepository)
     {
-        $this->productRepository = $productRepository;
-    }
-
-    public function all()
-    {
-        return $this->productRepository->all();
-    }
-
-    public function save(array $attributes)
-    {
-        return $this->productRepository->create($attributes);
-    }
-
-    public function update(array $attributes, int $id)
-    {
-        return $this->productRepository->update($id, $attributes);
-    }
-
-    public function destroy(int $id)
-    {
-        return $this->productRepository->destroy($id);
-    }
-
-    public function find(int $id)
-    {
-        return $this->productRepository->find($id);
+        parent::__construct($productRepository);
     }
 }
