f4c40be1c4813377ea750e1d564184ec6cf39f8e
pedro lucca <pedro.lucca@live.estacio.br>
2020-10-19 23:51:40 -0300

    feat: criando BaseController.php e métodos básicos

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use App\Http\Requests\ProductStoreRequest;
+use GuzzleHttp\Psr7\Request;
+
+class BaseController
+{
+    protected $service;
+    public function __construct(object $service)
+    {
+        $this->service = $service;
+    }
+
+    public function list(){
+        return $this->service->all();
+    }
+
+    public function store(Request $request)
+    {
+        return $this->service->save($request->all());
+    }
+
+    public function update(Request $request, $id)
+    {
+        return $this->service->update($request->all(), $id);
+    }
+
+    public function destroy($id)
+    {
+        return $this->service->destroy($id);
+    }
+
+    public function find(int $id)
+    {
+        return $this->service->find($id);
+    }
+}
