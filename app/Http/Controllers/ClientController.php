33cf39f02360c51f31420737f9e1badd7b0361fe
pedro lucca <pedro.lucca@live.estacio.br>
2020-10-19 23:52:23 -0300

    refactor: extendendo BaseController em ClientController

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -5,19 +5,16 @@ namespace App\Http\Controllers;
 use App\Services\ClientService;
 use Illuminate\Http\Request;
 
-class ClientController extends Controller
+class ClientController extends BaseController
 {
-    protected $clientService;
-
     public function __construct(ClientService $clientService)
     {
-        $this->clientService = $clientService;
+        parent::__construct($clientService);
     }
 
     public function index()
     {
-        $clients = $this->clientService->all();
-        return view('client.index')->with(["clients" => $clients]);
+        return view('client.index');
     }
 
     public function create()
@@ -25,36 +22,13 @@ class ClientController extends Controller
         return view('client.create');
     }
 
-    public function store(Request $request)
-    {
-        $this->clientService->save([
-            "name" => $request->get('name'),
-            "email" => $request->get('email')
-        ]);
-        return redirect('clients');
-    }
-
     public function show($id)
     {
-        $client = $this->clientService->find($id);
-        return view('client.show')->with(["client" => $client]);
+        return view('client.show');
     }
 
     public function edit($id)
     {
-        $client = $this->clientService->find($id);
-        return view('client.edit')->with(["client" => $client]);
-    }
-
-    public function update(Request $request, $id)
-    {
-        $this->clientService->update($request->all(), $id);
-        return redirect('clients');
-    }
-
-    public function destroy($id)
-    {
-        $this->clientService->destroy($id);
-        return redirect('clients');
+        return view('client.edit');
     }
 }
