53173bf56d51a88d0f0777afd6ba0f9a46aa2850
pedro lucca <pedro.lucca@live.estacio.br>
2020-10-19 23:57:09 -0300

    refactor: utilizando BaseController e removendo métodos não utilizados

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,59 +6,22 @@ use App\Http\Requests\ProductStoreRequest;
 use Illuminate\Http\Request;
 use \App\Services\ProductService;
 
-class ProductController extends Controller
+class ProductController extends BaseController
 {
-    protected $productService;
-
+    /**
+     * ProductController constructor.
+     * @param ProductService $productService
+     */
    public function __construct(ProductService $productService)
    {
-       $this->productService = $productService;
+       parent::__construct($productService);
    }
 
-    public function show($id)
-    {
-        $product = $this->productService->find($id);
-        return view('product.show')->with(["product" => $product]);
-    }
-
-    public function list()
-    {
-        return $this->productService->all();
-    }
-
+    /**
+     * view com todas as operações de crud (criar, editar, excluir e visualizar)
+     */
     public function index()
     {
         return view('product.index');
-    }
-
-    public function create()
-    {
-        return view('product.create');
-    }
-
-    public function store(ProductStoreRequest $request)
-    {
-        return $this->productService->save($request->all());
-    }
-
-    public function edit($id)
-    {
-        $product = $this->productService->find($id);
-        return view('product.edit')->with(["product" => $product]);
-    }
-
-    public function update(Request $request, $id)
-    {
-        return $this->productService->update($request->all(), $id);
-    }
-
-    public function destroy($id)
-    {
-        return $this->productService->destroy($id);
-    }
-
-    public function find($id)
-    {
-        return $this->productService->find((int) $id);
     }
 }
