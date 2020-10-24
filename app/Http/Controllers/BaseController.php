060e2f73586ca3f80db8140660439fabdc684db6
pedro lucca <pedro.lucca@live.estacio.br>
2020-10-24 09:38:06 -0300

    feat: inserindo phpdoc nas classes de base

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -6,33 +6,66 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/**
+ * Class BaseController
+ * @package App\Http\Controllers
+ */
 class BaseController
 {
+    /**
+     * @var object
+     */
     protected $service;
+
+    /**
+     * BaseController constructor.
+     * @param object $service
+     */
     public function __construct(object $service)
     {
         $this->service = $service;
     }
 
+    /**
+     * @return mixed
+     */
     public function index(){
         return $this->service->all();
     }
 
+    /**
+     * @param Request $request
+     * @return mixed
+     */
     public function store(Request $request)
     {
         return $this->service->save($request->all());
     }
 
+    /**
+     * @param Request $request
+     * @param $id
+     * @return mixed
+     */
     public function update(Request $request, $id)
     {
         return $this->service->update($request->all(), $id);
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function destroy($id)
     {
         return $this->service->destroy($id);
     }
 
+    /**
+     *
+     * @param int $id
+     * @return mixed
+     */
     public function find(int $id)
     {
         return $this->service->find($id);
--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -2,45 +2,85 @@
 
 namespace App\Repositories;
 
+/**
+ * Class BaseRepository
+ * @package App\Repositories
+ */
 class BaseRepository
 {
+    /**
+     * @var object
+     */
     protected $obj;
 
+    /**
+     * BaseRepository constructor.
+     * @param object $obj
+     */
     protected function __construct(object $obj)
     {
         $this->obj = $obj;
     }
 
+    /**
+     * @return object
+     */
     public function all(): object
     {
         return $this->obj->all();
     }
 
+    /**
+     * @param int $id
+     * @return object
+     */
     public function find(int $id): object
     {
         return $this->obj->find($id);
     }
 
+    /**
+     * @param string $column
+     * @param $value
+     * @return object
+     */
     public function findByColumn(string $column, $value): object
     {
         return $this->obj->where($column, $value)->get();
     }
 
+    /**
+     * @param array $attributes
+     * @return bool
+     */
     public function save(array $attributes): bool
     {
         return $this->obj->insert($attributes);
     }
 
+    /**
+     * @param int $id
+     * @param array $attributes
+     * @return bool
+     */
     public function update(int $id, array $attributes): bool
     {
         return $this->obj->find($id)->update($attributes);
     }
 
+    /**
+     * @param int $id
+     * @return bool
+     */
     public function destroy(int $id): bool
     {
         return $this->obj->destroy($id);
     }
 
+    /**
+     * @param array $attributes
+     * @return object
+     */
     public function create(array $attributes): object
     {
         return $this->obj->create($attributes);
