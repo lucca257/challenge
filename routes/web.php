07b8b443d227f389c707f88a96cb3411d6b94527
pedro lucca <pedro.lucca@live.estacio.br>
2020-10-23 22:51:28 -0300

    refactor: alterando nome da rota index para home de compras

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::prefix('orders')->group(function (){
     Route::get('create', [OrderController::class,'create'])->name("orders.create");
     Route::get('{order}', [OrderController::class,'show'])->name('orders.show');
     Route::get('{order}/edit', [OrderController::class,'edit'])->name('orders.edit');
-    Route::get('', [OrderController::class,'index'])->name('orders.index');
+    Route::get('', [OrderController::class,'index'])->name('orders.home');
     Route::post('', [OrderController::class,'store'])->name("orders.store");
     Route::patch('{order}', [OrderController::class,'update'])->name("orders.update");
     Route::delete('{order}', [OrderController::class,'destroy'])->name("orders.destroy");
