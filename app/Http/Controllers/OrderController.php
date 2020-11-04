b70ef6b0186b25c873163eb2b8515f6aa7ab55d7
pedro lucca <pedro.lucca@live.estacio.br>
2020-11-03 22:14:53 -0300

    refactor: padronizando controller e service

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,95 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\OrderStoreRequest;
-use App\Models\Order;
-use App\Models\OrderItem;
-use App\Services\ClientService;
+
 use App\Services\OrderItemService;
-use App\Services\OrderService;
-use App\Services\ProductService;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\View\View;
 
-class OrderController extends Controller
+/**
+ * Class OrderController
+ * @package App\Http\Controllers
+ */
+class OrderController extends BaseController
 {
-    protected $orderService, $clientService, $productService, $orderItemService;
-
-    public function __construct(OrderService $orderService, ClientService $clientService, ProductService $productService, OrderItemService $orderItemService)
+    /**
+     * OrderController constructor.
+     * @param OrderItemService $orderItemService
+     */
+    public function __construct(OrderItemService $orderItemService)
     {
-        $this->orderService = $orderService;
-        $this->clientService = $clientService;
-        $this->productService = $productService;
-        $this->orderItemService = $orderItemService;
-        //$this->middleware('auth');
+        parent::__construct($orderItemService);
     }
 
-    public function index()
+    /**
+     * @return Application|Factory|View
+     */
+    public function home(): View
     {
-        $orders = $this->orderItemService->all();
-        return view('order.index')->with(["orders" => $orders]);
-    }
-
-    public function create()
-    {
-        $clients = $this->clientService->all();
-        $products = $this->productService->all();
-        return view('order.create')->with([
-            "clients" => $clients,
-            "products" => $products
-        ]);
-    }
-
-    public function store(OrderStoreRequest $request)
-    {
-        $this->orderService->save($request->all());
-        return redirect('orders');
-    }
-
-    public function show($id)
-    {
-        $order = $this->orderItemService->find($id);
-        $clients = $this->clientService->all();
-        $products = $this->productService->all();
-        return view('order.show')->with([
-            "order" => $order,
-            "clients" => $clients,
-            "products" => $products
-        ]);
-    }
-
-    public function edit($id)
-    {
-        $order = $this->orderItemService->find($id);
-        $clients = $this->clientService->all();
-        $products = $this->productService->all();
-
-        return view('order.edit')->with([
-            "order" => $order,
-            "clients" => $clients,
-            "products" => $products
-        ]);
-    }
-
-    public function update(OrderStoreRequest $request, $id)
-    {
-        $this->orderItemService->update([
-            "product_id" => $request->get('product_id'),
-            "status" => $request->get('status'),
-            "quantity" => $request->get('quantity'),
-        ], $id);
-
-        $orderItem = $this->orderItemService->find($id);
-        $this->orderService->update([
-            'client_id' => $request->get('client_id')
-        ], $orderItem->order_id);
-
-        return redirect('orders')->with("success", "Pedido alterado com sucesso");
-    }
-
-    public function destroy($id)
-    {
-        $this->orderItemService->destroy($id);
-        return redirect()->back();
+        return view('order.index');
     }
 }
--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -8,46 +8,16 @@ use App\Repositories\OrderItemRepository;
 use App\Repositories\OrderRepository;
 use App\Repositories\ProductRepository;
 
-class OrderService
+class OrderService extends BaseService
 {
     protected $orderRepository, $productRepository, $orderItemRepository;
 
     public function __construct(OrderRepository $orderRepository, ProductRepository $productRepository, OrderItemRepository $orderItemRepository)
     {
+        parent::__construct($orderRepository);
         $this->orderRepository = $orderRepository;
         $this->productRepository = $productRepository;
         $this->orderItemRepository = $orderItemRepository;
-    }
-
-    public function save(array $attributes)
-    {
-        //saving first order
-        $order = $this->orderRepository->create([
-            'client_id' => $attributes["client_id"]
-        ]);
-        //creating order item
-        return $this->orderItemRepository->create([
-            'quantity' => $attributes["quantity"],
-            'price' => $this->calculateTotalPrice($attributes["product_id"],$attributes["quantity"]),
-            'status' => $attributes["status"],
-            'product_id' => $attributes["product_id"],
-            'order_id' => $order->id
-        ]);
-    }
-
-    public function update(array $attributes, int $id)
-    {
-        return $this->orderRepository->update($id, $attributes);
-    }
-
-    public function destroy(int $id)
-    {
-        return $this->orderRepository->destroy($id);
-    }
-
-    public function last()
-    {
-        return $this->orderRepository->last();
     }
 
     private function calculateTotalPrice(int $productId, int $totalItens){
