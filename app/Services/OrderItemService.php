ebd04fdcdd91171139b203d1d929211be7fcde9a
pedro lucca <pedro.lucca@live.estacio.br>
2020-11-08 02:08:43 -0300

    refactor: utilizando funções do BaseService.php

--- app/Services/OrderItemService.php
+++ app/Services/OrderItemService.php
@@ -6,31 +6,12 @@ namespace App\Services;
 
 use App\Repositories\OrderItemRepository;
 
-class OrderItemService
+class OrderItemService extends BaseService
 {
     protected $orderItemRepository;
 
     public function __construct(OrderItemRepository $orderItemRepository)
     {
-        $this->orderItemRepository = $orderItemRepository;
-    }
-
-    public function all()
-    {
-        return $this->orderItemRepository->all();
-    }
-
-    public function find(int $id){
-        return $this->orderItemRepository->find($id);
-    }
-
-    public function destroy(int $id)
-    {
-        return $this->orderItemRepository->destroy($id);
-    }
-
-    public function update(array $attributes, int $id)
-    {
-        return $this->orderItemRepository->update($id, $attributes);
+        parent::__construct($orderItemRepository);
     }
 }
