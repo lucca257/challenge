d25a5985f5b1b3dc68d015f94f7d2d292c07619c
pedro lucca <pedro.lucca@live.estacio.br>
2020-11-08 18:05:40 -0300

    refactor: buscando ordem e seus respectivos itens

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -12,6 +12,17 @@ class OrderRepository extends BaseRepository
 
     public function __construct(Order $order)
     {
+        $this->order = $order;
         parent::__construct($order);
+    }
+
+    /**
+     * find order and our itens
+     * @param int $id
+     * @return object
+     */
+    public function find(int $id): object
+    {
+        return $this->order->with('oderItem')->find($id);
     }
 }
