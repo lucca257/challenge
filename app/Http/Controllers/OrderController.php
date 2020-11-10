6f8b75b13e5cec6038447e9143c52f571ee144e2
pedro lucca <pedro.lucca@live.estacio.br>
2020-11-10 00:05:39 -0300

    feat: verificando se produto existe e realizando rollback em caso de erro

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 
 use App\Services\OrderItemService;
 use App\Services\OrderService;
+use App\Services\ProductService;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\View\View;
 
@@ -18,17 +20,19 @@ use Illuminate\View\View;
  */
 class OrderController extends BaseController
 {
-    private $orderService, $orderItemService;
+    private $orderService, $orderItemService, $productService;
 
     /**
      * OrderController constructor.
      * @param OrderService $orderService
      * @param OrderItemService $orderItemService
+     * @param ProductService $productService
      */
-    public function __construct(OrderService $orderService, OrderItemService $orderItemService)
+    public function __construct(OrderService $orderService, OrderItemService $orderItemService, ProductService $productService)
     {
         $this->orderService = $orderService;
         $this->orderItemService = $orderItemService;
+        $this->productService = $productService;
         parent::__construct($orderService);
     }
 
@@ -48,6 +52,7 @@ class OrderController extends BaseController
     public function store(Request $request): JsonResponse
     {
         try {
+            DB::beginTransaction();
             $order_request = $request->get('order');
             $item_request = $request->get('item');
             $order = $this->orderService->save([
@@ -57,15 +62,23 @@ class OrderController extends BaseController
 
             $save = [];
             foreach ($item_request as $item){
+                $product = $this->productService->find($item["product_id"]);
+                if (!$product) continue;
                 $save[] = $this->orderItemService->save([
                     "product_id" => $item["product_id"],
                     "quantity" => $item["product_id"],
                     "order_id" => $order->id
                 ]);
             }
-            return $this->responseApi(["order" => $order, "item" => $save], 'Dados criados com sucesso');
+            $response = ["order" => $order, "item" => $save];
+            if(empty($save)){
+                DB::rollBack();
+                return $this->responseApi([], 'nenhum item foi cadastrado', false, 500);
+            }
+            DB::commit();
+            return $this->responseApi($response, 'Dados criados com sucesso');
         } catch (\Exception $e) {
-            dd($e->getMessage());
+            DB::rollBack();
             Log::error($e->getMessage());
             return $this->responseApi([], 'Falha interna ao criar dados', false, 500);
         }
